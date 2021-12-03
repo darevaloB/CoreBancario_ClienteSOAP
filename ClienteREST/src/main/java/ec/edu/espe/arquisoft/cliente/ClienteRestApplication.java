524e6473aea878dd9f418b125e63130ab7c7a67d
Jose Andres Zamora Miranda <jazamora4@espe.edu.ec>
2021-12-03 00:18:00 -0500

    Cors

--- ClienteREST/src/main/java/ec/edu/espe/arquisoft/cliente/ClienteRestApplication.java
+++ ClienteREST/src/main/java/ec/edu/espe/arquisoft/cliente/ClienteRestApplication.java
@@ -2,12 +2,25 @@ package ec.edu.espe.arquisoft.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ClienteRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteRestApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
